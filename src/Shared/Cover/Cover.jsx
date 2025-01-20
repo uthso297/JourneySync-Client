6179aea6c7b4a214701f7eaebbceb9b026a81702
uthso297 <uthso119464@gmail.com>
2025-01-20 16:51:31 +0600

    added some animation in banner by framer motion

--- src/Shared/Cover/Cover.jsx
+++ src/Shared/Cover/Cover.jsx
@@ -1,5 +1,10 @@
+import { FacebookIcon, FacebookShareButton, TelegramIcon, TelegramShareButton, TwitterShareButton, WhatsappIcon, WhatsappShareButton, XIcon, } from 'react-share';
 import coverImg from '../../assets/Home/banner.jpg'
+import { motion } from "motion/react"
 const Cover = () => {
+
+    const shareUrl = 'https://github.com/uthso297'
+
     return (
         <div
             className="hero min-h-screen"
@@ -8,14 +13,63 @@ const Cover = () => {
             }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-neutral-content text-left absolute md:left-80">
-                <div className="max-w-md">
-                    <h1 className="mb-5 md:text-5xl text-4xl font-bold">Explore the world</h1>
-                    <h1 className="mb-5 md:text-5xl text-4xl font-bold">Make a difference</h1>
-                    <p className='text-lg mb-5'>Explore with ease and discover more—our Tourist Management System makes planning your perfect getaway simple, seamless, and stress-free!</p>
+                <div className="max-w-2xl">
+                    <motion.h1
+                        animate={{ y: ["-100vh", 0] }}
+                        transition={{ duration: 3, ease: "easeOut" }}
+                        className="mb-5 md:text-5xl text-3xl font-bold"
+                    >
+                        Explore the world
+                    </motion.h1>
+
+                    <div className="flex gap-3 items-center">
+                        <motion.button
+                            initial={{ opacity: 0.2 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 0.5, duration: 3, ease: "easeOut" }}
+                            className="btn bg-inherit sm:btn-sm md:btn-md lg:btn-lg text-white"
+                        >
+                            Get Started
+                        </motion.button>
+                        <motion.h1
+                            animate={{ y: ["-100vh", 0] }}
+                            transition={{ duration: 3, ease: "easeOut" }}
+                            className="mb-5 md:text-5xl text-3xl font-bold"
+                        >
+                            Make a difference
+                        </motion.h1>
+                    </div>
+                    <motion.p
+                        // animate={{ y: ["100vh", 0] }}
+                        animate={{ y: ["30vh", 0] }}
+                        transition={{ duration: 3, ease: "easeOut" }}
+                        className='md:text-lg mb-5'>Explore with ease and discover more—our Tourist Management System makes planning your perfect getaway simple, seamless, and stress-free!</motion.p>
                     {/* <button className="btn btn-primary">Get Started</button> */}
-                    <button className="btn btn-xs bg-inherit sm:btn-sm md:btn-md lg:btn-lg text-white">Get Started</button>
+
                 </div>
+
             </div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ x: [20, 0], opacity: 1 }}
+                transition={{ delay: 0.5, duration: 3, ease: "easeOut" }}
+                className='flex md:flex-col gap-5 absolute md:right-5 md:bottom-[40vh] bottom-10'>
+                <FacebookShareButton url={shareUrl}>
+                    <FacebookIcon size={32} round />
+                </FacebookShareButton>
+
+                <TwitterShareButton url={shareUrl}>
+                    <XIcon size={32} round />
+                </TwitterShareButton>
+
+                <TelegramShareButton url={shareUrl}>
+                    <TelegramIcon size={32} round />
+                </TelegramShareButton>
+
+                <WhatsappShareButton url={shareUrl}>
+                    <WhatsappIcon size={32} round />
+                </WhatsappShareButton>
+            </motion.div>
         </div>
     );
 };
