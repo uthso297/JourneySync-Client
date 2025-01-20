d273c16e93a41876aeb2d95afe78c4cadd595bed
uthso297 <uthso119464@gmail.com>
2025-01-21 00:43:48 +0600

    designed footer

---
+++ src/Shared/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import { FacebookIcon, FacebookShareButton, TelegramIcon, TelegramShareButton, TwitterShareButton, WhatsappIcon, WhatsappShareButton, XIcon } from 'react-share';
+const shareUrl = 'https://github.com/uthso297';
+const Footer = () => {
+    return (
+        <footer className="footer bg-base-200 text-base-content p-10">
+            <aside>
+                <img className='h-20 w-20 rounded-full' src="https://img.freepik.com/free-vector/detailed-travel-logo_23-2148616611.jpg?semt=ais_hybrid" alt="" />
+                <p>
+                    JourneySync
+                    <br />
+                    Providing reliable travel service since 2020
+                </p>
+            </aside>
+            <nav>
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Social Links</h6>
+                <FacebookShareButton url={shareUrl}>
+                    <FacebookIcon size={32} round />
+                </FacebookShareButton>
+
+                <TwitterShareButton url={shareUrl}>
+                    <XIcon size={32} round />
+                </TwitterShareButton>
+
+                <TelegramShareButton url={shareUrl}>
+                    <TelegramIcon size={32} round />
+                </TelegramShareButton>
+
+                <WhatsappShareButton url={shareUrl}>
+                    <WhatsappIcon size={32} round />
+                </WhatsappShareButton>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
