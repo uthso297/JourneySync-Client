a7c9ac5d16bf4c871a7bc664502b9972d20ebf6c
uthso297 <uthso119464@gmail.com>
2025-02-18 01:17:12 +0600

    made dashboard fixed position

--- src/Lauouts/Dashboard.jsx
+++ src/Lauouts/Dashboard.jsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
                     initial={{ opacity: 0 }}
                     animate={{ x: [-20, 0], opacity: 1 }}
                     transition={{ duration: 1, ease: "easeOut" }}
-                    className="w-full lg:w-1/5 md:min-h-screen bg-slate-800 p-4 lg:p-8">
+                    className="w-full lg:w-1/5 md:min-h-screen bg-slate-800 p-4 lg:p-8 lg:fixed">
                     <ul className="menu bg-base-200 rounded-box w-full">
                         {
                             isAdmin ? <>
@@ -60,11 +60,16 @@ const Dashboard = () => {
                     initial={{ opacity: 0 }}
                     animate={{ y: [20, 0], opacity: 1 }}
                     transition={{ duration: 1, ease: "easeOut" }}
-                    className="flex-1 p-4">
+                    className="flex-1 p-4 lg:pl-[20%]">
                     <Outlet />
                 </motion.div>
             </div>
-            <Footer />
+            {/* <Footer /> */}
+            <footer className="footer footer-center bg-base-300 text-base-content p-4">
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved by JourneySync</p>
+                </aside>
+            </footer>
         </>
     );
 };
